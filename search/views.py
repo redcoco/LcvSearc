614d7504095afb479e8b5757b1bc404d53ed216f
redcoco <1849737243@qq.com>
2017-09-05 21:52:19 +0800

    搜索记录、热门搜索功能实现

--- search/views.py
+++ search/views.py
@@ -1,47 +1,60 @@
+import json
 from django.shortcuts import render
 from django.views.generic.base import View
 from search.models import ArticleType
 from django.http import HttpResponse
-import json
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import redis
 
+client = Elasticsearch(hosts=["127.0.0.1"])
+redis_cli = redis.StrictRedis()
+
+
+class IndexView(View):
+    #首页
+    def get(self, request):
+        topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
+        return render(request, "index.html", {"topn_search":topn_search})
 
 # Create your views here.
 class SearchSuggest(View):
     def get(self, request):
-        key_words = request.GET.get('s', '')
+        key_words = request.GET.get('s','')
         re_datas = []
         if key_words:
             s = ArticleType.search()
-            # s = s.suggest('my_suggest', key_words, completion={
-            s = s.suggest('my-suggest', key_words, completion={
-                "field": "suggest", "fuzzy": {
-                    "fuzziness": 2
+            s = s.suggest('my_suggest', key_words, completion={
+                "field":"suggest", "fuzzy":{
+                    "fuzziness":2
                 },
                 "size": 10
             })
             suggestions = s.execute_suggest()
-            # for match in suggestions.my_suggest[0].options:
-            for match in getattr(suggestions,"my-suggest")[0].options:
+            for match in suggestions.my_suggest[0].options:
                 source = match._source
                 re_datas.append(source["title"])
         return HttpResponse(json.dumps(re_datas), content_type="application/json")
 
 
-from elasticsearch import Elasticsearch
-client = Elasticsearch(hosts="localhost")
-import datetime
-
 class SearchView(View):
-    def get(self,request):
+    def get(self, request):
         key_words = request.GET.get("q","")
-        page = request.GET.get("p","1")
+        s_type = request.GET.get("s_type", "article")
+
+        redis_cli.zincrby("search_keywords_set", key_words)
+
+        topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
+        page = request.GET.get("p", "1")
         try:
             page = int(page)
         except:
-            page=1
-        start_time = datetime.datetime.now()
+            page = 1
+
+        jobbole_count = redis_cli.get("jobbole_count")
+        start_time = datetime.now()
         response = client.search(
-            index = "jobbole",
+            index= "jobbole",
             body={
                 "query":{
                     "multi_match":{
@@ -49,7 +62,7 @@ class SearchView(View):
                         "fields":["tags", "title", "content"]
                     }
                 },
-                "from":0,
+                "from":(page-1)*10,
                 "size":10,
                 "highlight": {
                     "pre_tags": ['<span class="keyWord">'],
@@ -61,24 +74,25 @@ class SearchView(View):
                 }
             }
         )
-        end_time = datetime.datetime.now()
-        last_second = (end_time-start_time).total_seconds()
+
+        end_time = datetime.now()
+        last_seconds = (end_time-start_time).total_seconds()
         total_nums = response["hits"]["total"]
-        if (page%1)>0:
-            page_nums = int(total_nums/1 + 1)
+        if (page%10) > 0:
+            page_nums = int(total_nums/10) +1
         else:
-            page_nums = int(total_nums/1)
+            page_nums = int(total_nums/10)
         hit_list = []
         for hit in response["hits"]["hits"]:
-            hit_dict={}
+            hit_dict = {}
             if "title" in hit["highlight"]:
                 hit_dict["title"] = "".join(hit["highlight"]["title"])
             else:
                 hit_dict["title"] = hit["_source"]["title"]
             if "content" in hit["highlight"]:
-                hit_dict["content"] = "".join(hit["highlight"]["content"])
+                hit_dict["content"] = "".join(hit["highlight"]["content"])[:500]
             else:
-                hit_dict["content"] = hit["_source"]["content"]
+                hit_dict["content"] = hit["_source"]["content"][:500]
 
             hit_dict["create_date"] = hit["_source"]["create_date"]
             hit_dict["url"] = hit["_source"]["url"]
@@ -86,11 +100,11 @@ class SearchView(View):
 
             hit_list.append(hit_dict)
 
-        return render(request,"result.html",{
-                "all_hits": hit_list,
-                "key_words": key_words,
-                "page":page,
-            "total_nums":total_nums,
-            "page_nums":page_nums,
-            "last_second":last_second
-            })
+        return render(request, "result.html", {"page":page,
+                                               "all_hits":hit_list,
+                                               "key_words":key_words,
+                                               "total_nums":total_nums,
+                                               "page_nums":page_nums,
+                                               "last_seconds":last_seconds,
+                                               "jobbole_count":jobbole_count,
+                                               "topn_search":topn_search})
