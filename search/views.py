cdeda6355555c9249db04e6d260984ba7aa2c8de
redcoco <1849737243@qq.com>
2017-09-05 18:29:59 +0800

    django实现elasticsearch的搜索功能

--- search/views.py
+++ search/views.py
@@ -25,3 +25,56 @@ class SearchSuggest(View):
                 source = match._source
                 re_datas.append(source["title"])
         return HttpResponse(json.dumps(re_datas), content_type="application/json")
+
+
+from elasticsearch import Elasticsearch
+client = Elasticsearch(hosts="localhost")
+
+class SearchView(View):
+    def get(self,request):
+        key_words = request.GET.get("q","")
+        response = client.search(
+            index = "jobbole",
+            body={
+                "query":{
+                    "multi_match":{
+                        "query":key_words,
+                        "fields":["tags", "title", "content"]
+                    }
+                },
+                "from":0,
+                "size":10,
+                "highlight": {
+                    "pre_tags": ['<span class="keyWord">'],
+                    "post_tags": ['</span>'],
+                    "fields": {
+                        "title": {},
+                        "content": {},
+                    }
+                }
+            }
+        )
+        total_nums = response["hits"]["total"]
+        hit_list = []
+        for hit in response["hits"]["hits"]:
+            hit_dict={}
+            if "title" in hit["highlight"]:
+                hit_dict["title"] = "".join(hit["highlight"]["title"])
+            else:
+                hit_dict["title"] = hit["_source"]["title"]
+            if "content" in hit["highlight"]:
+                hit_dict["content"] = "".join(hit["highlight"]["content"])
+            else:
+                hit_dict["content"] = hit["_source"]["content"]
+
+            hit_dict["create_date"] = hit["_source"]["create_date"]
+            hit_dict["url"] = hit["_source"]["url"]
+            hit_dict["score"] = hit["_score"]
+
+            hit_list.append(hit_dict)
+
+        return render(request,"result.html",{
+                "all_hits": hit_list,
+                "key_words": key_words
+
+            })
